a50ae8b9e496bce1b02c303e4498af0c237bcc7e
Cameron-Stock <clstock33@gmail.com>
2020-12-03 20:16:22 -0600

    Added user confirmation

--- searchword.py
+++ searchword.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,14 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if No: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word does not exist. Please double check the word, and try again."
+        else:
+            return "We did not understand your entry"
     else:
         return "The word does not exist. Please double check the word, and try again."
 
